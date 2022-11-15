0a7052ddf5f9976b937c69378eb54060682a02c4
amdoolittle <amdoolittle@gmail.com>
2022-11-15 00:41:21 -0700

    Update pwm_fan_control.py
    Add signal handling and rework if/elif logic

--- pwm_fan_control.py
+++ pwm_fan_control.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from signal import *
 import lgpio
 import time
 
@@ -7,28 +8,29 @@ PWM_HZ = 10000 # Max HZ in lgpio library is 10K
 
 h = lgpio.gpiochip_open(1)
 
+def clean():
+    lgpio.gpiochip_close(h)
+
+signal(Signals.SIGTERM, clean)
+
 while(1):
-     # Get CPU temp
-     file = open("/sys/class/thermal/thermal_zone0/temp")
-     temp = float(file.read()) / 1000.00
-     temp = float('%.2f' % temp)
-     file.close()
+    last = 0
+    # Get CPU temp
+    file = open("/sys/class/thermal/thermal_zone0/temp")
+    temp = float(file.read()) / 1000.00
+    temp = float('%.2f' % temp)
+    file.close()
 
-     if(temp > 30):
-          lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 40)
-
-     if(temp > 50):
-          lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 50)
-
-     if(temp > 55):
-          lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 75)
-
-     if(temp > 60):
-          lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 90)
-
-     if(temp > 65):
-          lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 100)
-
-     if(temp < 30):
-          lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 0)
-     time.sleep(1)
+    if(temp > 65):
+         lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 100)
+    elif(temp > 60):
+         lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 90)
+    elif(temp > 55):
+         lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 75)
+    elif(temp > 50):
+         lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 50)
+    elif(temp > 30):
+         lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 40)
+    elif(temp <= 30):
+         lgpio.tx_pwm(h, FAN_PIN, PWM_HZ, 0)
+    time.sleep(10)
