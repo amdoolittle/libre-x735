85caf7f1bceb6fc1e52d161f03497bc4adddfb82
amdoolittle <amdoolittle@gmail.com>
2022-11-22 23:51:13 -0700

    Reorg, added power management script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-svcloc=/etc/systemd/system/pwmfan.service
+svcloc=/etc/systemd/system
 
-sed -e 's,scriptpath,'"$(pwd)"',g' pwmfan.service > $svcloc
+sed -e 's,scriptpath,'"$(pwd)"',g' x735fan.service > $svcloc/x735fan.service
+sed -e 's,scriptpath,'"$(pwd)"',g' x735pwr.service > $svcloc/x735pwr.service
 
 systemctl daemon-reload
-systemctl enable pwmfan.service
+systemctl enable x735fan.service
+systemctl enable x735pwr.service
---
+++ x735pwr.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+SHUTDOWN=497
+# Reboot if .2-2sec, shutdown if more
+REBOOTPULSE=200
+SHUTDOWNPULSE=2000
+
+echo "$SHUTDOWN" > /sys/class/gpio/export
+echo "in" > /sys/class/gpio/gpio$SHUTDOWN/direction
+BOOT=496
+echo "$BOOT" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio$BOOT/direction
+echo "1" > /sys/class/gpio/gpio$BOOT/value
+
+echo "X735 Waiting for button press..."
+
+while [[ 1 ]]; do
+  shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
+  if [[ $shutdownSignal -eq 0 ]]; then
+    /bin/sleep 0.2
+  else
+    pulseStart=$(date +%s)
+    while [[ $shutdownSignal -eq 1 ]]; do
+      /bin/sleep 0.2
+      if [[ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $SHUTDOWNPULSE ]]; then
+        echo "X735 Shutting down..."
+        sudo /usr/sbin/poweroff
+        exit
+      fi
+      shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
+    done
+    if [[ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSE ]]; then
+      echo "X735 Rebooting..."
+      sudo /usr/sbin/reboot
+      exit
+    fi
+  fi
+done
